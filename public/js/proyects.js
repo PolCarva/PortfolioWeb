028243892c3f3be1aa752f0675fbcf148a7d4b8e
PolCarva <107940742+PolCarva@users.noreply.github.com>
2023-06-14 15:37:13 -0300

    Proyecto Agregado

--- public/js/proyects.js
+++ public/js/proyects.js
@@ -9,9 +9,37 @@ const c4dLogo = "C4D";
 const processingLogo = "processing";
 const nodeLogo = "node";
 const resolumeLogo = "resolume";
-
+const flutter = "flutter";
+const react = "react";
+const ionic = "ionic";
+const bootstrap = "bootstrap";
+const firebase = "firebase";
 
 const proyects = [
+  {
+    title: "Dropper",
+    type: "web",
+    categories: "WEBSITE, MOBILE APP, BASE DE DATOS, ECOMMERCE",
+    year: "2023",
+    color: "#0F4A79",
+    img: "Dropper",
+    filter: "Websites",
+    info: "Dropper es un Ecommerce ficticio hecho en React, creado por mi en el curso de CoderHouse, con el cual quedé dentro del top 10 entre más de 100 alumnos.",
+    url: "https://droppershop.netlify.app/",
+    habilities: [cssLogo, react, bootstrap, firebase],
+  },
+  {
+    title: "Coinvert",
+    type: "mobile",
+    categories: "WEBSITE, MOBILE APP, API",
+    year: "2023",
+    color: "#815496",
+    img: "Coinvert",
+    filter: "Websites",
+    info: "Coinvert es una página para convertir Crytpocurrencies en Monedas Tradicionales y viceversa, trackear el valor historico de la moneda que desees y ver su evolución semanal. Proyecto creado con APIS en 2023 para la materia Creaccion de Aplicaciónes",
+    url: "https://coinvert-hazel.vercel.app/",
+    habilities: [htmlLogo, cssLogo, jsLogo, bootstrap],
+  },
   {
     title: "Sushi App",
     type: "mobile",
@@ -48,7 +76,6 @@ const proyects = [
     info: "Movie Me! es un proyecto desarrollado en HTML, CSS y JavaScript que consiste en una página web para buscar películas y crear una lista de favoritos o pendientes. El proyecto fue creado en 2022 como parte de una asignatura de Programación 2. La página web está integrada con la API TMDb, que permite a los usuarios buscar películas por título, género o actor. Además, los usuarios pueden crear una cuenta en el sitio y almacenar sus listas de películas favoritas o pendientes en un servidor propio. La página web cuenta con una interfaz de usuario intuitiva y fácil de usar, lo que permite a los usuarios navegar por la página, buscar películas y agregarlas a sus listas con facilidad. En resumen, Movie Me! es un proyecto útil para los amantes del cine, que permite a los usuarios encontrar y organizar películas de manera efectiva, y también fue una excelente oportunidad para aprender sobre el consumo y creación de APIs.",
     url: "https://movie-me.glitch.me/",
     habilities: [htmlLogo, cssLogo, jsLogo],
-
   },
 
   {
@@ -62,7 +89,6 @@ const proyects = [
     info: "RARA es una página web creada en 2022 como proyecto para la asignatura de Programación 1. La página web ofrece una versión personalizada de Zara, con una interfaz de usuario moderna y elegante que permite a los usuarios navegar por los diferentes productos y categorías. La página web fue desarrollada utilizando HTML, CSS y JavaScript, y los usuarios pueden buscar y encontrar fácilmente los productos que desean utilizando un filtro de búsqueda por nombre. Los productos se presentan de manera clara y organizada, con detalles como imágenes, precios y descripciones, lo que permite a los usuarios conocer más sobre los productos que están interesados. En general, RARA fue una gran experiencia para mejorar mis habilidades y aprender sobre el diseño y la implementación de páginas web modernas y atractivas sin necesidad de implementar backend.",
     url: "proyect-file/Rara/index.html",
     habilities: [htmlLogo, cssLogo, jsLogo],
-
   },
 
   {
@@ -76,7 +102,6 @@ const proyects = [
     info: "BlackJack es un proyecto que consiste en un juego de cartas en el que los jugadores compiten contra una máquina. Fue creado en 2022 como proyecto para la asignatura de Programación 2, y se desarrolló utilizando técnicas de consumo de APIs. El juego es similar al blackjack clásico, en el que el jugadore intenta obtener una mano de cartas con un valor lo más cercano posible a 21, sin excederlo. El juego es interactivo y permite a los jugadores realizar apuestas, recibir cartas y decidir si quieren plantarse, pedir otra carta o rendirse. La máquina juega automáticamente y utiliza un algoritmo para decidir cuándo plantarse o pedir más cartas. El juego cuenta con una interfaz de usuario clara y fácil de usar, y fue  una forma divertida de aprender sobre las APIs y su consumo en proyectos de programación. En general, BlackJack fue un excelente proyecto para mejorar las habilidades de programación.",
     url: "proyect-file/BlackJack/index.html",
     habilities: [htmlLogo, cssLogo, jsLogo],
-
   },
 
   {
@@ -90,8 +115,6 @@ const proyects = [
     info: "ChePibe es un proyecto que consiste en el diseño en Figma de una aplicación móvil de delivery de favores. El objetivo de la aplicación es conectar a los usuarios con un delivery que pueda ayudarlos en lo que necesiten, desde realizar compras hasta hacer tareas cotidianas. Los usuarios pueden hablar con los deliveries y pactar un precio por sus servicios. El diseño de la aplicación se enfoca en la experiencia del usuario y la facilidad de uso, con una interfaz intuitiva y clara que permite a los usuarios encontrar rápidamente los servicios que necesitan. Además, el proyecto también cuenta con una landing page de presentación que muestra las características y beneficios de la aplicación. ChePibe fue creado para la asignatura de Diseño Digital 5 en 2022, y fue una excelente oportunidad para mejorar las habilidades de diseño UX/UI, ya que se enfoca en la experiencia del usuario y la facilidad de uso. En general, ChePibe es un proyecto interesante que muestra cómo se pueden aplicar los principios de diseño de UX/UI para crear una aplicación móvil que ofrezca una experiencia satisfactoria a los usuarios.",
     url: "https://www.figma.com/proto/GIdEALtLeI0Kxj8dmFrZIJ/ChePibe?page-id=0%3A1&node-id=53%3A642&viewport=415%2C381%2C0.11&scaling=scale-down&starting-point-node-id=8%3A5",
     habilities: [figmaLogo],
-
-  
   },
 
   {
@@ -105,7 +128,6 @@ const proyects = [
     info: "Fitó es un proyecto de branding que se enfoca en el diseño de identidad corporativa para una marca de un esrudio de diseño multimedia. El objetivo del proyecto es unificar las ideas de naturaleza con las de tecnología, y crear una marca que transmita una imagen fresca y moderna. El diseño de la identidad corporativa se basa en un esquema de colores naturales y vibrantes, con una paleta de verdes y violetas que representan la naturaleza y el ambiente, combinado con formas geométricas que reflejan la tecnología y la innovación. La identidad corporativa también incluye diseños de papelería, merchandising y otros materiales de marketing. En general, Fitó es un proyecto interesante que muestra cómo se puede unificar conceptos opuestos en un diseño de branding coherente y efectivo, y es un ejemplo excelente de cómo aplicar los principios del diceño para una marca personal.",
     url: "https://www.figma.com/proto/tG1ixhca5a7oskBoXgdy5S/Identidad-Corporativa?page-id=0%3A1&node-id=98%3A3&viewport=-11%2C129%2C0.03&scaling=scale-down&starting-point-node-id=98%3A3",
     habilities: [figmaLogo],
-
   },
 
   {
@@ -119,7 +141,6 @@ const proyects = [
     info: "Catfishing es un corto audiovisual creado en 2022 como parte de la materia Lenguaje Audiovisual. La historia del corto gira en torno a una chica que comienza a hablar con un hombre a través de internet, sin saber que está siendo engañada. A medida que la historia se desarrolla, se utiliza el lenguaje audiovisual para crear una atmósfera misteriosa y tensa, que refleja lo que siente el espectador, que en todo momento sabe más que la protagonista. Se utilizan recursos visuales y de sonido para transmitir la sensación de engaño y para crear una narrativa emotiva y cautivadora. En general, Catfishing es un excelente ejemplo de cómo el lenguaje audiovisual puede utilizarse para contar historias efectivas, emocionantes y sorprendentes, y cómo la combinación adecuada de elementos visuales y sonoros puede transmitir emociones y sentimientos complejos en una corta duración.",
     url: "https://youtu.be/DcCC_iPgrAU",
     habilities: [prLogo, aeLogo],
-
   },
 
   {
@@ -133,7 +154,6 @@ const proyects = [
     info: "API Clima es un proyecto que consiste en una web que utiliza el consumo de la API weathermap para brindar información sobre el clima en tiempo real. Además, según las condiciones climáticas actuales, la web muestra partículas de lluvia, niebla o nieve para dar una experiencia más inmersiva al usuario. También cuenta con la funcionalidad de detectar si es de día o de noche, y proporciona una breve información sobre la próxima festividad del país que se está buscando. El proyecto fue creado originalmente para la materia Programación 2, sin embargo, debido a su poca versatilidad, terminó siendo sustituido. En general, el proyecto API Clima muestra cómo se puede utilizar una API para brindar información en tiempo real y cómo se pueden agregar detalles adicionales para mejorar la experiencia del usuario.",
     url: "proyect-file/Api Clima/frontend/index.html",
     habilities: [htmlLogo, cssLogo, jsLogo],
-
   },
 
   {
@@ -147,7 +167,6 @@ const proyects = [
     info: "Este proyecto consiste en una serie de videos cortos creados con herramientas básicas de Cinema4D. Fue mi primer acercamiento al mundo del 3D y me permitió aprender las habilidades necesarias para crear y animar objetos tridimensionales. Los videos pueden variar en contenido, desde simples animaciones de objetos hasta escenas más elaboradas con partículas y luces. Este proyecto fue creado en 2021 para la materia de Diseño y Animación 3D, y permitió al creador explorar las posibilidades creativas del software y desarrollar habilidades en animación y diseño 3D.",
     url: "https://www.behance.net/gallery/164255459/Diseno-y-Animacion-3D",
     habilities: [c4dLogo],
-
   },
 
   {
@@ -174,6 +193,5 @@ const proyects = [
     info: "El proyecto de videomapping realizado en Resolume Arena consistió en proyectar figuras y animaciones en una estructura en forma de pirámide compuesta por 3 cajas. Se utilizaron diversos videos y algunas animaciones propias, para crear efectos visuales llamativos y atractivos. La presentación del proyecto se realizó en un espacio oscuro para resaltar los efectos de iluminación generados por las proyecciones, lo que creó una experiencia inmersiva y sorprendente para el espectador. El proyecto fue desarrollado como parte del curso de Taller de Arte Digital 1 en el año 2021.",
     url: "https://youtu.be/I2mU4VFsy1U",
     habilities: [resolumeLogo],
-
   },
 ];
